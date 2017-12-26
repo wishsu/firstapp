14dbdda10129e650c78326d0d97832c8f1da7caa
wishsu <ap1251f8x@aliyun.com>
2017-12-26 07:08:08 +0000

    hello.py upgrade

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-<h1>Hello, world!</h1>
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello,World!"
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
